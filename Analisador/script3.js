bc963c28ea4d4886c8225b7bdcae58c8c5a30b34
Thaynara Antunes <thay_antunes@icloud.com>
2024-08-15 04:21:07 -0300

    finalizado

--- Analisador/script3.js
+++ Analisador/script3.js
@@ -1,6 +1,6 @@
 var num = document.getElementById('num')
 var sel = document.getElementById('sel')
-var res = document.getElementById('contar')
+var res = document.getElementById('res')
 var valores = []
 
 function isNum(n) {
@@ -22,8 +22,43 @@ function inSel(n, l) {
 
 function contar() {
     if(isNum(num.value) && !inSel(num.value, valores)) {
-        window.alert('tudo ok')
+        valores.push(Number(num.value))
+        var item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado.`
+        sel.appendChild(item)
+        res.innerHTML = ''
+       if (valores.length === 1) {
+            sel.options[0].remove()
+        }
     } else {
         window.alert('Valor Inválido ou já foi adicionado.')
+    }
+    num.value = ''
+    num.focus()
+}
+
+function analisar() {
+    if(valores.length == 0) {
+        window.alert('Adicione valores antes de continuar.')
+    } else {
+        var total = valores.length
+        var maior = valores[0]
+        let menor = valores[0]
+        var soma = 0
+        var media = 0
+        for(var pos in valores) {
+            soma += valores[pos]
+            if (valores[pos] > maior)
+                maior = valores[pos]
+            if (valores[pos] < menor)
+                menor = valores[pos]
+        }
+        media = soma / total
+        
+        res.innerHTML += `<p>Ao todo temos ${total} números cadastrados.</p>`
+        res.innerHTML += `<p>O menor valor informado foi ${menor}.</p>`
+        res.innerHTML += `<p>O maior valor informado foi ${maior}. </p>`
+        res.innerHTML += `<p>A soma de todos os valores foi de ${soma}.`
+        res.innerHTML += `<p>A média dos valores é de ${media}</p>`
     }
 }
